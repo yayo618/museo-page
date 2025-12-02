7d984b36334831bdf1ffc5d884145104e4807cc3
yayo <jorgefloresari@gmail.com>
2025-12-02 12:21:20 -0500

    url

--- script.js
+++ script.js
@@ -7,4 +7,11 @@ renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+var cube = new THREE.Mesh(
+	new THREE.BoxGeometry(1,1,1),
+	new THREE.MeshBasicMaterial({color:0x00ff00, wireframe:true})
+);
+cube.position.z = -2;
+scene.add(cube);
+
 renderer.render(scene,camera);
