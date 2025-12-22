09d8d6c42db9b1842f5ad499f98596d3aef8c67f
yayo <jorgefloresari@gmail.com>
2025-12-22 00:39:49 -0500

    prueba cam-perspectiva cuadros del frente

--- cuadros/cuadros.js
+++ cuadros/cuadros.js
@@ -1,25 +1,27 @@
-var txt1 = new THREE.TextureLoader().load("cuadros/c1.jpg");
 var cuadroGeo =  new THREE.PlaneGeometry(1.4,0.88);
-var cuadroMat =  new THREE.MeshBasicMaterial({color:0xffffff,map:txt1});
 var cuadroBG = new THREE.BoxGeometry(1.4,0.88,0.05,0);
-var cuadroBM = new THREE.MeshBasicMaterial({color:0x46284B, wireframe:true});
+var cuadroBM = new THREE.MeshBasicMaterial({color:0x46284B});
 var cuadroWG = new THREE.PlaneGeometry(1.4,0.05);
 var cuadroWM = new THREE.MeshBasicMaterial({color:0xff0000,wireframe:true});
 
-//cuadro1
-var cdr1 = new THREE.Mesh(cuadroGeo, cuadroMat);
-//borde cuadro 1
-var cdrB1 = new THREE.Mesh(cuadroBG, cuadroBM);
-cdr1.position.set(0,0,-3.7);
-cdrB1.position.set(0,0,-3.72);
-scene.add(cdr1,cdrB1);
 
 for (let i = 0; i < 4; i++){//arriba
+	/*
 	//wire cuadro
 	var wireC = new THREE.Mesh(cuadroWG,cuadroWM);
 	wireC.rotation.x = Math.PI/2;//gira para ver wire
 	wireC.position.set(-3+i*2,0,-3.72);
 	scene.add(wireC);
+	*/
+var txt1 = new THREE.TextureLoader().load(`cuadros/${i+1}.jpg`);
+var cuadroMat =  new THREE.MeshBasicMaterial({color:0xffffff,map:txt1});
+//cuadro1
+var cdr1 = new THREE.Mesh(cuadroGeo, cuadroMat);
+//borde cuadro 1
+var cdrB1 = new THREE.Mesh(cuadroBG, cuadroBM);
+cdr1.position.set( -3+i*2,0,-3.7 );
+cdrB1.position.set(-3+i*2,0,-3.72);
+scene.add(cdr1,cdrB1);
 }
 for (let i = 0; i < 4; i++){//izq
 	//wire cuadro
--- js/script.js
+++ js/script.js
@@ -30,7 +30,7 @@ var helper = new THREE.Mesh(
 );
 helper.rotation.x += Math.PI/2;
 //helper.rotation.z = -Math.PI/2;
-scene.add(helper);
+//scene.add(helper);//para ver desde arriba con la otra c
 
 var ptGeo = new THREE.PlaneGeometry(7.5,8.8,10,10);
 var txtPiso = new THREE.TextureLoader();
@@ -38,7 +38,7 @@ var pisoTxt = new txtPiso.load("models/piso.png");
 var piso = new THREE.Mesh(
     ptGeo,
     new THREE.MeshBasicMaterial({color:0xffffff, map:pisoTxt}));
-piso.rotation.x = Math.PI/2;//camera orto no lo ve
+piso.rotation.x = -Math.PI/2;//camera orto no lo ve cambiar +-
 piso.rotation.z = Math.PI/2;
 piso.position.y = -1.5;
 var txtTicho = new THREE.TextureLoader();
@@ -74,8 +74,8 @@ pared3.position.set(4.4, -0.375, 0);
 pared3.rotation.y = -Math.PI/2;
 pared4.position.set(-4.4, -0.375, 0);
 pared4.rotation.y = Math.PI/2;
-//scene.add(piso, ticho, pared1, pared2, pared3, pared4);
-scene.add(piso, pared1, pared2, pared3, pared4);
+scene.add(piso, ticho, pared1, pared2, pared3, pared4);
+//scene.add(piso, pared1, pared2, pared3, pared4);
 //aqui estaban cuadro
 
 var geo = new THREE.BoxGeometry(1,1,1);
@@ -245,7 +245,7 @@ paredLoader.load("models/paredes.mtl", function(materials){
 
 var animate = function(){
 	requestAnimationFrame(animate);
-	renderer.render(scene, newcamera);
+	renderer.render(scene, camera);
 	mueve();
 }
 animate();
