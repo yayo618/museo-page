6c6a824daa0c5c5804f9467b132b420a17fa810d
yayo <jorgefloresari@gmail.com>
2025-12-22 00:55:37 -0500

    12 cuadros agregados correctamente

--- cuadros/cuadros.js
+++ cuadros/cuadros.js
@@ -13,29 +13,55 @@ for (let i = 0; i < 4; i++){//arriba
 	wireC.position.set(-3+i*2,0,-3.72);
 	scene.add(wireC);
 	*/
-var txt1 = new THREE.TextureLoader().load(`cuadros/${i+1}.jpg`);
+var txt1 = new THREE.TextureLoader().load(`cuadros/${i+5}.jpg`);
 var cuadroMat =  new THREE.MeshBasicMaterial({color:0xffffff,map:txt1});
 //cuadro1
 var cdr1 = new THREE.Mesh(cuadroGeo, cuadroMat);
 //borde cuadro 1
 var cdrB1 = new THREE.Mesh(cuadroBG, cuadroBM);
 cdr1.position.set( -3+i*2,0,-3.7 );
-cdrB1.position.set(-3+i*2,0,-3.72);
+cdrB1.position.set(-3+i*2,0,-3.725);
 scene.add(cdr1,cdrB1);
 }
 for (let i = 0; i < 4; i++){//izq
+	/*
 	//wire cuadro
 	var wireC = new THREE.Mesh(cuadroWG,cuadroWM);
 	wireC.rotation.x = Math.PI/2;
 	wireC.rotation.z = Math.PI/2;
 	wireC.position.set(-4.375,0,-2.7+i*1.8);
 	scene.add(wireC);
+	*/
+var txt1 = new THREE.TextureLoader().load(`cuadros/${i+1}.jpg`);
+var cuadroMat =  new THREE.MeshBasicMaterial({color:0xffffff,map:txt1});
+//cuadro1
+var cdr1 = new THREE.Mesh(cuadroGeo, cuadroMat);
+//borde cuadro 1
+var cdrB1 = new THREE.Mesh(cuadroBG, cuadroBM);
+cdr1.rotation.y = Math.PI/2;
+cdrB1.rotation.y = Math.PI/2;
+cdr1.position.set( -4.37+0.025 ,0,-2.7+i*1.8 );
+cdrB1.position.set(-4.37,0,-2.7+i*1.8);
+scene.add(cdr1,cdrB1);
 }
 for (let i = 0; i < 4; i++){//der
+	/*
 	//wire cuadro
 	var wireC = new THREE.Mesh(cuadroWG,cuadroWM);
 	wireC.rotation.x = Math.PI/2;//gira para ver wire
 	wireC.rotation.z = Math.PI/2;
 	wireC.position.set(4.375,0,-2.7+i*1.8);
 	scene.add(wireC);
+	*/
+var txt1 = new THREE.TextureLoader().load(`cuadros/${i+9}.jpg`);
+var cuadroMat =  new THREE.MeshBasicMaterial({color:0xffffff,map:txt1});
+//cuadro1
+var cdr1 = new THREE.Mesh(cuadroGeo, cuadroMat);
+//borde cuadro 1
+var cdrB1 = new THREE.Mesh(cuadroBG, cuadroBM);
+cdr1.rotation.y = -Math.PI/2;
+cdrB1.rotation.y = Math.PI/2;
+cdr1.position.set( 4.37-0.025 ,0,-2.7+i*1.8 );
+cdrB1.position.set(4.37,0,-2.7+i*1.8);
+scene.add(cdr1,cdrB1);
 }
